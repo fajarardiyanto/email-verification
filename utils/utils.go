af74e8fc9525de22d76e4b5a97094f367230c232
fajarardiyanto <fajarardiyanto.web@gmail.com>
2021-08-01 13:01:06 +0700

    utils add generate random string

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"math/rand"
+	"strings"
+
 	"golang.org/x/crypto/bcrypt"
 )
+
+const letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func HashedPassword(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -10,4 +15,14 @@ func HashedPassword(password string) ([]byte, error) {
 
 func VerifyPassword(hashed, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+}
+
+func GenerateRandomString(n int) string {
+	sb := strings.Builder{}
+	sb.Grow(n)
+	for i := 0; i < n; i++ {
+		idx := rand.Int63() % int64(len(letter))
+		sb.WriteByte(letter[idx])
+	}
+	return sb.String()
 }
