5553e96b9701dffb50013ca775e087e427eb7994
fajarardiyanto <fajarardiyanto.web@gmail.com>
2021-08-08 17:52:14 +0700

    set init & routing

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"context"
+	"mail-service/controllers"
+	"mail-service/global"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/qiniu/qmgo"
+)
+
+var dbName = "DB"
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	dbName = os.Getenv("DB_NAME")
+	initMongoDB()
+}
+
+func initMongoDB() {
+	if client, err := qmgo.NewClient(context.TODO(), &qmgo.Config{Uri: os.Getenv("MONGO_URI")}); err != nil {
+		panic(err)
+	} else {
+		global.MainDB = client.Database(dbName)
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/signup", controllers.SignUp)
+
+	r.Run(os.Getenv("BIND_ADDR"))
 }
