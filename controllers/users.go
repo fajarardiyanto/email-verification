4ab6d71b0367add47173f47464b99910c7bb6a44
fajarardiyanto <fajarardiyanto.web@gmail.com>
2021-08-08 17:52:58 +0700

    fix user interface & verify code

--- controllers/users.go
+++ controllers/users.go
@@ -2,15 +2,23 @@ package controllers
 
 import (
 	"context"
+	"github.com/google/uuid"
+	"mail-service/global"
 	"mail-service/service"
 	"mail-service/structs"
 	"mail-service/utils"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func signUp(c *gin.Context) {
+type AuthHandler struct {
+	mailService service.MailService
+}
+
+func SignUp(c *gin.Context) {
+	var ah *AuthHandler
 	user := structs.User{}
 
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -25,23 +33,27 @@ func signUp(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   true,
-			"message": "Failed created password", err.Error(),
+			"message": "Failed created password" + err.Error(),
 		})
 		return
 	}
 
-	user.Passwrod = hashedPassword
+	id := uuid.New().String()
+	user.ID = id
+	user.Password = hashedPassword
 	user.TokenHash = utils.GenerateRandomString(15)
+	user.CreatedAt = time.Now().Unix()
 
-	if err = initApp.mainDB.Collection("users").InsertOne(context.TODO(), structs.User{}); err != nil {
+	_, err = global.MainDB.Collection("users").InsertOne(context.TODO(), user)
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   true,
-			"message": "Failed created password", err.Error(),
+			"message": "Failed created password" + err.Error(),
 		})
 		return
 	}
 
-	from := "vikisquarez@gmail.com"
+	from := "testingtrash13@gmail.com"
 	to := []string{user.Email}
 	subject := "Email Verification for Bookite"
 	mailType := service.MailConfirmation
@@ -49,4 +61,39 @@ func signUp(c *gin.Context) {
 		Username: user.Username,
 		Code:     utils.GenerateRandomString(8),
 	}
+
+	mailReq := ah.mailService.NewMail(from, to, subject, mailType, mailData)
+	err = ah.mailService.SendMail(mailReq)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error to send mail",
+			"error":   err.Error(),
+			"data":    "",
+		})
+		return
+	}
+
+	verification := &structs.VerificationData{
+		UserID:    id,
+		Email:     user.Email,
+		Code:      mailData.Code,
+		Type:      structs.MailConfirmation,
+		ExpiresAt: time.Now().Add(time.Hour * time.Duration(1)),
+	}
+
+	_, err = global.MainDB.Collection("users_verification").InsertOne(context.TODO(), verification)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error to store mail verification data",
+			"error":   err.Error(),
+			"data":    "",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Please verify your email account using the confirmation code send to your mail",
+		"error":   "",
+		"data":    verification,
+	})
 }
